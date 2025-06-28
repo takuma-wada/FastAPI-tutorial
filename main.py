e01aa92a835344139f56762e37e5812d244afcd2
takuma-wada <9th.takuma@gmail.com>
2025-06-28 14:34:20 +0900

    パスパラメータ

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from typing import Dict, Any
+from enum import Enum
+
+# Enum class
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+# FastAPIアプリケーションのインスタンスを作成
+app = FastAPI()
+
+# APIドキュメント(openapi) エンドポイント
+# /redoc or /docs
+
+# ルートエンドポイント
+@app.get("/")
+async def read_root() -> Dict[str, str]:
+    return {"message": "こんにちは, FastAPI!"}
+
+# パスパラメータを持つエンドポイント
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None) -> Dict[str, Any]:
+    return {"item_id": item_id, "q": q}
+
+# enumで型アノテーションしたエンドポイント
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet: # 比較
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet": # model_name.valueでも取得可能
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
