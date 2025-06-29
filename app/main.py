a1a799f5478bfa7a28094b08a65eb96f8afdf10f
takuma-wada <9th.takuma@gmail.com>
2025-06-29 16:30:00 +0900

    リクエストボディ

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from typing import Dict, Any, Union
+from typing import Dict, Any
 from enum import Enum
 from pydantic import BaseModel
+
 
 # Enum class
 class ModelName(str, Enum):
@@ -9,37 +10,67 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+
+# リクエストbody sample パラメーターと考え方は同じ version 3.10
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
 # FastAPIアプリケーションのインスタンスを作成
 app = FastAPI()
 
 # APIドキュメント(openapi) エンドポイント
 # /redoc or /docs
 
+
 # ルートエンドポイント
 @app.get("/")
 async def read_root() -> Dict[str, str]:
     return {"message": "こんにちは, FastAPI!"}
+
 
 # パスパラメータを持つエンドポイント
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str | None = None) -> Dict[str, Any]:
     return {"item_id": item_id, "q": q}
 
+
 # enumで型アノテーションしたエンドポイント
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet: # 比較
+    if model_name is ModelName.alexnet:  # 比較
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-    if model_name.value == "lenet": # model_name.valueでも取得可能
+    if model_name.value == "lenet":  # model_name.valueでも取得可能
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
+
 # クエリパラメーター
 @app.get("/user/items/{item_id}")
 async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
-): # Noneでオプショナル、なければ必須。デフォルト値指定も可能
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):  # Noneでオプショナル、なければ必須。デフォルト値指定も可能
     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
+
+
+# リクエストbody
+@app.post("/items/")
+async def create_item(item: Item):
+    # item.name.capitalize //pydanticを受け取るとエディターによる型ヒントと補完が関数内で利用可能
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+# リクエストbody + パスパラメータ(上と同じ要領でクエリパラメータも指定できる)
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
